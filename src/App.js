ffd54b4778b64a5b8437a6c11418bb6d2884a293
ARC\mdiedrich <diedrich.moritz@arc-greenlab.de>
2022-07-14 14:50:06 +0200

    working with states

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
 
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th, 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th, 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 7th, 2:30pm',
+        reminder: false,
+    },
+])
+
   return (
     <div className="container">
-
       <Header title='kuzgkhfgkf' />
+      <Tasks tasks={tasks}/>
     </div>
 
   );
 }
-
-
-
 
 export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,14 @@
+
+
+export const Tasks = (props) => {
+    
+    
+
+    return (
+        <>
+            {props.tasks.map((task) => <h3 key={task.id}>{task.text}</h3>)}
+        </>
+    )
+}
+
+export default Tasks
